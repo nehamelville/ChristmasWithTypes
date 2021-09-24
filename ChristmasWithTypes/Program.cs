17543960d2a6630447ce1bc3fb5cb5bbaa3d4646
nehamelville <neha.melville@gmail.com>
2021-09-24 11:43:14 -0700

    code modified

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"car","Iphone","book"}; 
+            xmas.Presents =  new string[3]{"Xbox","Nintendo Switch","Iphone X"}; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
